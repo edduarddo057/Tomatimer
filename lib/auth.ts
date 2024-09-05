e81e266bdfd83437ead7f08761468eb0441c7ef0
Eduardo <edduarddo057@gmail.com>
2024-09-04 22:11:42 -0300

    fix: bug in production secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 
 export const nextAuthOptions: NextAuthOptions = {
+    secret: "ebc24059-92f1-4564-b5ba-e42eac106b2f",
     providers: [CredentialsProvider({
         name: "credentials",
         credentials: {
